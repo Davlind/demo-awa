6224ea3e65a5e40607fdd9d767350906748693de
David Lindblad <dalind@microsoft.com>
2017-10-08 18:21:02 +0200

    Added apps insights

--- viewpoint/Program.cs
+++ viewpoint/Program.cs
@@ -19,6 +19,7 @@ namespace viewpoint
 
         public static IWebHost BuildWebHost(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
+                .UseApplicationInsights()
                 .UseStartup<Startup>()
                 .Build();
     }
